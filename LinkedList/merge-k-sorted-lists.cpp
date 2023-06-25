6a56dece984aa4f76f6dd408dfab87daea8faf86
Manish kumar <manishmandal9734@gmail.com>
2023-06-25 22:20:08 +0530

    update

---
+++ LinkedList/merge-k-sorted-lists.cpp
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//  Definition for singly-linked list.
+struct ListNode
+{
+    int val;
+    ListNode *next;
+    ListNode(int x) : val(x), next(NULL) {}
+};
+
+class mycompare
+{
+public:
+    bool operator()(const ListNode *a, const ListNode *b)
+    {
+        return (a->val > b->val);
+    }
+};
+
+class Solution
+{
+public:
+    ListNode *mergeKLists(vector<ListNode *> &lists)
+    {
+        ListNode *head = new ListNode(0);
+        ListNode *temp = head;
+        priority_queue<ListNode *, vector<ListNode *>, mycompare> pq;
+        for (int i = 0; i < lists.size(); i++)
+        {
+            if (lists[i] != NULL)
+                pq.push(lists[i]);
+        }
+
+        while (!pq.empty())
+        {
+            auto least = pq.top();
+            pq.pop();
+            temp->next = least;
+            temp = temp->next;
+            if (least->next)
+            {
+                pq.push(least->next);
+            }
+        }
+        return head->next;
+    }
+};
+
+int main(){
+    ListNode *head = new ListNode()
+}
